b79da8a02a6b7b8459af9ae08c79204833118c87
TEXTRAX28 <natanaelrehuels@gmail.com>
2025-04-03 00:14:02 +0100

    added 5_Post_Game_Stats.py

---
+++ Intermidiate Python/6_Dear_Diary/6_Dear_Diary.py
@@ -0,0 +1,8 @@
+with open("test.txt", 'w') as file:
+    file.write("This is entry 1\n")
+    file.write("This is entry 2\n")
+    file.write("This is entry 3\n")
+    file.write("This is entry 4\n")
+
+print("Entry success")
+print("Testing ")
