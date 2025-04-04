03a495550a3f922108cfd5610628ccb1e5d9499f
TEXTRAX28 <natanaelrehuels@gmail.com>
2025-04-04 01:59:31 +0100

    updated 13_Grammys.py

--- Intermidiate Python/13_Grammys.py
+++ Intermidiate Python/13_Grammys.py
@@ -1,4 +1,6 @@
 # List of songs with their durations (in minutes)
+from functools import reduce
+
 playlist = [('What Was I Made For?', 3.42),
             ('Just Like That', 5.05),
             ('Song 3', 6.8),
@@ -12,14 +14,21 @@ def duration_more_than_5(song):
 def minute_to_second(minute):
     return minute[1]*60
 
-def add_all_duration(add):
-    return add[1] + add[2] + add[3]
+def add_all_duration(total, add):
+    return total + add
 
 filter_playlist = list(filter(duration_more_than_5, playlist))
 print(filter_playlist)
 
 map_playlist = list(map(minute_to_second, playlist))
 print(map_playlist)
+
+durations = [song[1] for song in playlist]
+
+total_minutes = reduce(add_all_duration, durations)
+print(total_minutes)
+
+
 
 
 
